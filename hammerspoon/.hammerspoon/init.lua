190f906c183e39e4a4d4a22daa5618cd431c42ca
Jared Norman <jared@super.gd>
2020-08-30 15:29:56 -0700

    More hammerspoon updates

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,42 +1,17 @@
--- This moves windows into position for streaming.
-hs.hotkey.bind({"cmd", "alt", "shift"}, "M", function()
-  local mode = hs.screen.mainScreen():currentMode()
-  local frame = hs.screen.mainScreen():frame()
-  local barHeight = mode.h - frame.h
-
-  local width = 1006
-  local height = 900
-
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-
-  local padding = (frame.h - height) / 2
-
-  f.y = barHeight + padding
-  f.x = padding
-  f.w = width
-  f.h = height
-
-  win:setFrame(f)
-end)
-
-XPADDING = 31
-YPADDING = XPADDING - 1.5
-
 -- This moves windows into my preferred screen position on my desktop.
 hs.hotkey.bind({"cmd", "alt", "shift"}, "`", function()
   local mode = hs.screen.mainScreen():currentMode()
   local frame = hs.screen.mainScreen():frame()
   local barHeight = mode.h - frame.h
 
-  local width = frame.w - 2 * XPADDING
-  local height = frame.h - 2 * YPADDING
+  local width = frame.w
+  local height = frame.h
 
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
-  f.y = barHeight + YPADDING
-  f.x = XPADDING
+  f.y = barHeight
+  f.x = 0
   f.w = width
   f.h = height
 
